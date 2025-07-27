a799dc9ffd572073fa81fbf576793ef433997f15
ceung2025 <cc.medtools@gmail.com>
2025-07-27 22:11:34 +0700

    Add .next/static/chunks/src_app_private_page_tsx_a993527f._.js 1753629093280

---
+++ .next/static/chunks/src_app_private_page_tsx_a993527f._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_aaee5046._.js"
+],
+    source: "dynamic"
+});
